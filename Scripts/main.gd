9b133ca961de983d57db1995dc316ed9f2445070
0versc0re <115696138+0versc0re@users.noreply.github.com>
2022-10-19 19:17:15 +1030

    .

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,12 +2,21 @@ extends Node2D
 
 export (Vector2) var spawn = Vector2(0, 0)
 export (float) var zoom = 0.7
+var menu = false
 
 func _ready():
 	Global.player = Network.instance_player(Network.id, spawn)
 	$Camera2D.position = spawn
 
 func _process(delta):
+	if menu == true:
+		$Camera2D/Node2D/menu.visible = true
+		$Camera2D/Node2D/options.visible = true
+		$Camera2D/Node2D/Dark.visible = true
+	if menu == false:
+		$Camera2D/Node2D/menu.visible = false
+		$Camera2D/Node2D/options.visible = false
+		$Camera2D/Node2D/Dark.visible = false
 	$Camera2D.position += (Global.player.position - $Camera2D.position)/5
 	if Input. is_action_pressed("zoom_in"):
 		zoom -= 0.01 * zoom
@@ -15,3 +24,16 @@ func _process(delta):
 		zoom += 0.01 * zoom
 	zoom = clamp(zoom, 0.1, 1.5)
 	$Camera2D.zoom = Vector2(zoom, zoom)
+	$Camera2D/Node2D.scale = $Camera2D.zoom
+	if Input. is_action_just_pressed("in_game_menu"):
+		if menu == false:
+			menu = true
+		else:
+			menu = false
+
+func _on_menu_pressed():
+	get_tree().change_scene("res://Scenes/Menu.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene("res://Scenes/options_in_game.tscn")
--- Scripts/options.gd
+++ Scripts/options.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_back_pressed():
-	Global.changeScene("Menu")
+	get_tree().change_scene("res://Scenes/main.tscn")
 	
 func _ready():
 	$Username.text = Network.databaseData["username"]
