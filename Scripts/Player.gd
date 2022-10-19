1e7e6c8695b0fb3310b787b0d84a8a214dbc0002
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-10-19 19:13:33 +1030

    e

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -132,7 +132,9 @@ func _on_FloorDetect_body_exited(body):
 		onFloor = false
 
 func _on_EnemyDetect_body_entered(body):
-	velocity.x -= 100
+	if "slime" in body.name or "Blue_jumper" in body.name:
+		velocity.x = -2000*$Visual.scale.x
+		velocity.y = -250
 
 func _on_tick_rate_timeout():
 	if name == Network.id:
