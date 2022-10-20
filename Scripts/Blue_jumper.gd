71a36afa38d293c8a10fc7bc9a7a1363aca6cffa
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-10-20 18:32:17 +1030

    e

--- Scripts/Blue_jumper.gd
+++ Scripts/Blue_jumper.gd
@@ -4,14 +4,16 @@ export var speed = 10
 export var jump = 500
 export var gravity = 50
 
+var animateTimer = 0
+
 var velocity = Vector2(0, 0)
 
 
 func _physics_process(delta):
-	$BlueJumperSpriteSheet.frame = 0
-	yield(get_tree().create_timer(1),"timeout")
-	$BlueJumperSpriteSheet.frame = 1
-	yield(get_tree().create_timer(1),"timeout")
-	$BlueJumperSpriteSheet.frame = 2
-	yield(get_tree().create_timer(1),"timeout")
-	#yield(get_tree().create_timer(2),"timeout")
+	animateTimer += delta
+	if animateTimer >= 0.25:
+		animateTimer = 0
+		if $BlueJumperSpriteSheet.frame == 2:
+			$BlueJumperSpriteSheet.frame = 0
+		else:
+			$BlueJumperSpriteSheet.frame += 1
