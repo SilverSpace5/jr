ab94d3636877d10ecfa091cd19c37256bea279c6
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-10-20 16:33:37 +1030

    e

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,18 +6,15 @@ var menu = false
 var consoleWait = 0
 
 func _ready():
+	$Camera2D/Scale/Menu.visible = true
 	Global.player = Network.instance_player(Network.id, spawn)
 	$Camera2D.position = spawn
 
 func _process(delta):
 	if menu == true:
-		$Camera2D/Node2D/menu.visible = true
-		$Camera2D/Node2D/options.visible = true
-		$Camera2D/Node2D/Dark.visible = true
+		$Camera2D/Scale/Menu.visible = true
 	if menu == false:
-		$Camera2D/Node2D/menu.visible = false
-		$Camera2D/Node2D/options.visible = false
-		$Camera2D/Node2D/Dark.visible = false
+		$Camera2D/Scale/Menu.visible = false
 	$Camera2D.position += (Global.player.position - $Camera2D.position)/5
 	if Input. is_action_pressed("zoom_in"):
 		zoom -= 0.01 * zoom
@@ -25,7 +22,7 @@ func _process(delta):
 		zoom += 0.01 * zoom
 	zoom = clamp(zoom, 0.1, 1.5)
 	$Camera2D.zoom = Vector2(zoom, zoom)
-	$Camera2D/Node2D.scale = $Camera2D.zoom
+	$Camera2D/Scale.scale = $Camera2D.zoom
 	consoleWait -= delta
 	if Console.focus:
 		consoleWait = 0.1
