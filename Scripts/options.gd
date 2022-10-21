43fccffb3c554772f42b12281b2fa91b2b6b03a1
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-10-21 20:46:25 +1030

    e

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -9,7 +9,9 @@ func _on_back_pressed():
 	
 func _ready():
 	$Username.text = Network.databaseData["username"]
+	$Volume.value = Network.databaseData["volume"]
 	$Id.text = Global.id
 
 func _process(delta):
 	Network.databaseData["username"] = $Username.text
+	Network.databaseData["volume"] = $Volume.value
